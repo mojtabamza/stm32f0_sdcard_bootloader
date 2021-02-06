c01305eac5f88607d8bbdc370c75929afac148a2
mojtabamza <mojtaba.zamani.65@gmail.com>
2021-02-06 14:02:13 +0330

    add all files of project to that

---
+++ MDK-ARM/RTE/_boot_f030CC/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'boot_f030CC' 
+ * Target:  'boot_f030CC' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/boot_f030CC/startup_stm32f030xc.d
@@ -0,0 +1,1 @@
+boot_f030cc\startup_stm32f030xc.o: startup_stm32f030xc.s
